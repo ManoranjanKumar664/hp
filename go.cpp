ea1818b369e638ab783ae7ca0d50dc712aea2b14
Manoranjan Kumar <baskeymanoranjan400@gmail.com>
2022-09-05 11:24:30 +0530

    added

---
+++ go.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+cout<<"hello world";
+getch();
+}
+
+void mano()
+{
+cout<<"manoranjan";
+}
+
+void ranjan()
+{
+cout<<"baskey";
+}
